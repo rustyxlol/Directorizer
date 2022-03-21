5f0cb8c5a018dc8461af4f7dcb1d3ca58eb3a797
Rusty <imkhazix@gmail.com>
2022-03-21 20:21:31 +0530

    better indents, fixed some minor input issues

--- directorize.py
+++ directorize.py
@@ -5,41 +5,42 @@ import shutil
 
 
 def getFiles(src):
-    return [i for i in os.listdir(src) if os.path.isfile(os.path.join(src, i))]
+  return [i for i in os.listdir(src) if os.path.isfile(os.path.join(src, i))]
 
 
 def getMatches(fileName, categories):
-    return [match for match in categories if match in fileName.lower()]
+  return [match for match in categories if match in fileName.lower()]
 
 
 def createDirectories(src, categories):
-    for category in categories:
-        dirName = os.path.join(src, category)
-        try:
-            os.makedirs(dirName)
-        except OSError:
-            print("File already exists!")
-            pass
+  for category in categories:
+    dirName = os.path.join(src, category)
+    try:
+      os.makedirs(dirName)
+    except OSError:
+      print("File already exists!")
+      pass
 
 
 def moveFile(src, dst):
-    if os.path.isfile(src):
-        shutil.move(src, dst)
+  if os.path.isfile(src):
+    shutil.move(src, dst)
 
 
 if __name__ == "__main__":
-    src = input("Enter source path: ")
-    input_categories = input("Enter categories separated by comma(a,b,c) : ").split(",")
+  src = input("Enter source path: ")
+  input_categories = input(
+      "Enter categories separated by comma(a,b,c): ").split(",")
 
-    categories = [i.lower() for i in input_categories]
-    createDirectories(src, categories)
+  categories = [i.lower().strip() for i in input_categories if i]
+  createDirectories(src, categories)
 
-    fileNames = getFiles(src)
+  fileNames = getFiles(src)
 
-    for fileName in fileNames:
-        matches = getMatches(fileName, categories)
-        if matches:
-            srcFile = os.path.join(src, fileName)
-            dstFile = os.path.join(src, matches[0], fileName)
-            moveFile(srcFile, dstFile)
-    print("Finished the job boss")
+  for fileName in fileNames:
+    matches = getMatches(fileName, categories)
+    if matches:
+      srcFile = os.path.join(src, fileName)
+      dstFile = os.path.join(src, matches[0], fileName)
+      moveFile(srcFile, dstFile)
+  print("Finished the job boss")
